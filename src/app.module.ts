3a82a2b1cbfdd39ad2000bbfddc3c66f02fe9037
Marcus <mvsilva1997@gmail.com>
2021-09-28 22:30:49 -0300

    commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FilmesModule } from './filmes/filmes.module';
+import { ParticipantesModule } from './participantes/participantes.module';
+import { GenerosModule } from './generos/generos.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [FilmesModule, ParticipantesModule, GenerosModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
